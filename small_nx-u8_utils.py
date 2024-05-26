7331005391e15f0cd8949b9a044597fd6ca7c4ce
Uồầy hợp lý quãi chất thế nhờ✓✓✓ <148612010+901D3@users.noreply.github.com>
2024-05-26 09:44:29 +0700

    Update small_nx-u8_utils.py

--- small_nx-u8_utils.py
+++ small_nx-u8_utils.py
@@ -98,21 +98,70 @@ def dti_2():
         asm_code_pA = asm_code_input[:2]
         asm_code_pB = asm_code_input[2:]
 
+
+        if asm_code_input.lower() == "q":
+            break
+
+        if asm_code_input.lower() == "e":
+            print("Completed Assembly Instructions:")
+            for instruction in instructions:
+                print(instruction)
+            instructions = []  # Clear the list for the next use
+            continue  # Continue to accept more inputs
+
+        if len(asm_code_input) < 4:
+            print("Invalid assembly code")
+            continue
+
+        if not all(c in "0123456789ABCDEFabcdef" for c in asm_code_input):
+            print("Only hexadecimal characters are allowed")
+            continue
+
+        asm_code_input = asm_code_input.upper()
+        asm_code_pA = asm_code_input[:2]
+        asm_code_pB = asm_code_input[2:]
+
         #-----------------------------------------------------------------
-        #Rn
+        # MOV Rn, #xxH
 
         if asm_code_pA in "00 01 02 03 04 05 06 07 08 09".split(" "):
             MOV = f"MOV R{int(asm_code_pA[1], 16)}, #{asm_code_pB}H"
             instruction = f"{asm_code_input} -> {MOV}"
             print(instruction)
 
-        elif asm_code_pA in "0A 0B 0C 0D 0E 0F".split(" "):
+        elif asm_code_pA in ["0A", "0B", "0C", "0D", "0E", "0F"]:
             MOV = f"MOV R{int(asm_code_pA[1], 16)}, #{asm_code_pB}H"
             instruction = f"{asm_code_input} -> {MOV}"
             print(instruction)
 
         #-----------------------------------------------------------------
-        #ERn
+        # MOV Rn, Rm
+
+        elif (asm_code_pA in "80 81 82 83 84 85 86 87 88 89".split(" ")) and (asm_code_pB in "00 10 20 30 40 50 60 70 80 90".split(" ") or asm_code_pB in ["A0", "B0", "C0", "D0", "E0", "F0"]):
+            MOV = f"MOV R{int(asm_code_pA[1], 16)}, R{int(asm_code_pB[0], 16)}"
+            instruction = f"{asm_code_input} -> {MOV}"
+            print(instruction)
+
+        elif (asm_code_pA in ["8A", "8B", "8C", "8D", "8E", "8F"]) and (asm_code_pB in "00 10 20 30 40 50 60 70 80 90".split(" ") or asm_code_pB in ["A0", "B0", "C0", "D0", "E0", "F0"]):
+            MOV = f"MOV R{int(asm_code_pA[1], 16)}, R{int(asm_code_pB[0], 16)}"
+            instruction = f"{asm_code_input} -> {MOV}"
+            print(instruction)
+
+        #-----------------------------------------------------------------
+        # CMP Rn, #xxH
+
+        elif asm_code_pA in "70 71 72 73 74 75 76 77 78 79".split(" "):
+            CMP = f"CMP R{int(asm_code_pA[1], 16)}, #{asm_code_pB}H"
+            instruction = f"{asm_code_input} -> {CMP}"
+            print(instruction)
+
+        elif asm_code_pA in ["7A", "7B", "7C", "7D", "7E", "7F"]:
+            CMP = f"CMP R{int(asm_code_pA[1], 16)}, #{asm_code_pB}H"
+            instruction = f"{asm_code_input} -> {CMP}"
+            print(instruction)
+
+        #-----------------------------------------------------------------
+        # MOV ERn, #xxH
 
         elif asm_code_pA in "E0 E2 E4 E6 E8".split(" "):
             MOV = f"MOV ER{int(asm_code_pA[1], 16)}, #{asm_code_pB}H"
@@ -125,7 +174,33 @@ def dti_2():
             print(instruction)
 
         #-----------------------------------------------------------------
-        #PUSH Rn
+        # MOV ERn, ERm
+
+        elif (asm_code_pA in ["F0", "F2", "F4", "F6", "F8"] and asm_code_pB in "00 20 40 60 80".split(" ")) or asm_code_pB in ["A0", "C0", "E0"]:
+            MOV = f"MOV ER{int(asm_code_pA[1], 16)}, ER{int(asm_code_pB[0], 16)}"
+            instruction = f"{asm_code_input} -> {MOV}"
+            print(instruction)
+
+        elif asm_code_pA in ["FA", "FC", "FE"] and (asm_code_pB in "00 20 40 60 80".split(" ") or asm_code_pB in ["A0", "C0", "E0"]):
+            MOV = f"MOV ER{int(asm_code_pA[1], 16)}, ER{int(asm_code_pB[0], 16)}"
+            instruction = f"{asm_code_input} -> {MOV}"
+            print(instruction)
+
+        #-----------------------------------------------------------------
+        # CMP ERn, ERm
+
+        elif asm_code_pA in ["F0", "F2", "F4", "F6", "F8"]:
+            CMP = f"CMP ER{int(asm_code_pA[1], 16)}, #{asm_code_pB}H"
+            instruction = f"{asm_code_input} -> {CMP}"
+            print(instruction)
+
+        elif asm_code_pA in ["7A", "7C", "7E"]:
+            CMP = f"CMP ER{int(asm_code_pA[1], 16)}, #{asm_code_pB}H"
+            instruction = f"{asm_code_input} -> {CMP}"
+            print(instruction)
+
+        #-----------------------------------------------------------------
+        # PUSH Rn
 
         elif asm_code_pA in "F0 F1 F2 F3 F4 F5 F6 F7 F8 F9".split(" ") and asm_code_pB == "4E":
             PUSH = f"PUSH R{int(asm_code_pA[1], 16)}"
@@ -138,19 +213,20 @@ def dti_2():
             print(instruction)
 
         #-----------------------------------------------------------------
-        #POP Rn
+        # POP Rn
 
         elif asm_code_pA in "F0 F1 F2 F3 F4 F5 F6 F7 F8 F9".split(" ") and asm_code_pB == "0E":
-            PUSH = f"POP R{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP R{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
 
         elif asm_code_pA in ["FA", "FB", "FC", "FD", "FE", "FF"] and asm_code_pB == "0E":
-            PUSH = f"POP R{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP R{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
+
         #-----------------------------------------------------------------
-        #PUSH ERn
+        # PUSH ERn
 
         elif asm_code_pA in "F0 F2 F4 F6 F8".split(" ") and asm_code_pB == "5E":
             PUSH = f"PUSH ER{int(asm_code_pA[1], 16)}"
@@ -163,20 +239,20 @@ def dti_2():
             print(instruction)
 
         #-----------------------------------------------------------------
-        #POP ERn
+        # POP ERn
 
         elif asm_code_pA in "F0 F2 F4 F6 F8".split(" ") and asm_code_pB == "1E":
-            PUSH = f"POP ER{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP ER{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
 
         elif asm_code_pA in ["FA", "FC", "FE"] and asm_code_pB == "1E":
-            PUSH = f"POP ER{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP ER{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
 
         #-----------------------------------------------------------------
-        #PUSH XRn
+        # PUSH XRn
 
         elif asm_code_pA in "F0 F4 F8".split(" ") and asm_code_pB == "6E":
             PUSH = f"PUSH XR{int(asm_code_pA[1], 16)}"
@@ -189,20 +265,20 @@ def dti_2():
             print(instruction)
 
         #-----------------------------------------------------------------
-        #POP XRn
+        # POP XRn
 
         elif asm_code_pA in "F0 F4 F8".split(" ") and asm_code_pB == "2E":
-            PUSH = f"POP XR{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP XR{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
 
         elif asm_code_pA in "FC" and asm_code_pB == "2E":
-            PUSH = f"POP XR{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP XR{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
 
         #-----------------------------------------------------------------
-        #PUSH QRn
+        # PUSH QRn
 
         elif asm_code_pA in "F0 F8".split(" ") and asm_code_pB == "7E":
             PUSH = f"PUSH QR{int(asm_code_pA[1], 16)}"
@@ -215,29 +291,52 @@ def dti_2():
             print(instruction)
 
         #-----------------------------------------------------------------
-        #POP QRn
+        # POP QRn
 
         elif asm_code_pA in "F0 F8".split(" ") and asm_code_pB == "3E":
-            PUSH = f"POP QR{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP QR{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
 
         elif asm_code_pA in "FC" and asm_code_pB == "3E":
-            PUSH = f"POP QR{int(asm_code_pA[1], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
-            print(instruction)
-        #-----------------------------------------------------------------
-        #MOV Rn, Rm
-
-        elif asm_code_pA in "80 81 82 83 84 85 86 87 88 89".split(" ") and asm_code_pB in "00 10 20 30 40 50 60 70 80 90".split(" ") or ["A0", "B0", "C0", "D0", "E0", "F0"]:
-            PUSH = f"MOV R{int(asm_code_pA[1], 16)}, R{int(asm_code_pB[0], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
+            POP = f"POP QR{int(asm_code_pA[1], 16)}"
+            instruction = f"{asm_code_input} -> {POP}"
             print(instruction)
 
-        elif asm_code_pA in ["8A", "8B", "8C", "8D", "8E", "8F"] and asm_code_pB in "00 10 20 30 40 50 60 70 80 90".split(" ") ["A0", "B0", "C0", "D0", "E0", "F0"]:
-            PUSH = f"MOV R{int(asm_code_pA[1], 16)}, R{int(asm_code_pB[0], 16)}"
-            instruction = f"{asm_code_input} -> {PUSH}"
-            print(instruction)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
